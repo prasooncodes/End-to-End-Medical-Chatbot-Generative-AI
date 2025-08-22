ec7d1272a5dae39d8657adc3a33d345eebe3587a
Prasoon <prasoonmishra9936@gmail.com>
2025-08-22 23:44:25 +0530

    Add initial project structure with setup, requirements, and empty files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "end-to-end-medical-chatbot",
+    version = "0.0.0",
+    author = "Prasoon Mishra",
+    author_email = "prasoonmishra99362gmail.com",
+    packages = find_packages(),
+    install_requires = []
+
+)
